116d80e0243ea5348eaa1b36399103c4cce64415
Giacomo <giacomoaaron@gmail.com>
2015-10-04 01:07:57 -0400

    gio

---
+++ FrontEnd/javascript/profile.js
@@ -0,0 +1,50 @@
+$( ".theweeknd" ).show(0);
+
+$( ".theweeknd" ).click(function() {
+$( ".theweeknd" ).hide( "slow" );
+});
+$( ".2" ).show(0);
+
+$( ".2" ).click(function() {
+$( ".2" ).hide( "slow" );
+});
+$( ".3" ).show(0);
+
+$( ".3" ).click(function() {
+$( ".3" ).hide( "slow" );
+});
+$( ".4" ).show(0);
+
+$( ".4" ).click(function() {
+$( ".4" ).hide( "slow" );
+});
+function addInput(divName){
+  var newdiv = document.createElement("div");
+  newdiv.innerHTML = " <li class='4'><input type='text' name='addInputs[]' size='30' maxlength='30' class='4'> </li>";
+  document.getElementById('divName').appendChild(newdiv);
+}
+$( ".1" ).show(0);
+
+$( ".1" ).click(function() {
+$( ".1" ).hide( "slow" );
+});
+$( ".5" ).show(0);
+
+$( ".5" ).click(function() {
+$( ".5" ).hide( "slow" );
+});
+$( ".6" ).show(0);
+
+$( ".6" ).click(function() {
+$( ".6" ).hide( "slow" );
+});
+$( ".7" ).show(0);
+
+$( ".7" ).click(function() {
+$( ".7" ).hide( "slow" );
+});
+function Input(divName2){
+  var newdiv2 = document.createElement("div");
+  newdiv2.innerHTML = " <li class='7'><input type='text' size='30' maxlength='30' class='7'></li>";
+  document.getElementById('divName2').appendChild(newdiv2);
+}
